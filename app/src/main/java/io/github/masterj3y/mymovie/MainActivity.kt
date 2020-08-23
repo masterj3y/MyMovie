bd5b1f2558cfea887e92de97c4aaf2e11b12d20e
Mohammad Jahangiry <mjahangiry75@gmail.com>
2020-08-23 23:29:51 +0430

    Add Fragment Navigation

--- app/src/main/java/io/github/masterj3y/mymovie/MainActivity.kt
+++ app/src/main/java/io/github/masterj3y/mymovie/MainActivity.kt
@@ -1,11 +1,74 @@
 package io.github.masterj3y.mymovie
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ncapdevi.fragnav.FragNavController
+import com.ncapdevi.fragnav.FragNavSwitchController
+import com.ncapdevi.fragnav.FragNavTransactionOptions
+import com.ncapdevi.fragnav.tabhistory.UniqueTabHistoryStrategy
+import dagger.hilt.android.AndroidEntryPoint
+import io.github.masterj3y.mymovie.core.platform.BaseActivity
+import io.github.masterj3y.mymovie.core.platform.BaseFragment
+import io.github.masterj3y.mymovie.databinding.ActivityMainBinding
+import io.github.masterj3y.mymovie.movie.search.SearchMovieFragment
+import me.ibrahimsn.lib.OnItemReselectedListener
 
-class MainActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class MainActivity : BaseActivity<ActivityMainBinding>(), BaseFragment.FragmentNavigationHost {
+
+    private val fragNavController: FragNavController by lazy {
+        FragNavController(supportFragmentManager, R.id.fragmentContainer)
+    }
+
+    override fun layoutRes() = R.layout.activity_main
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        initFragNavController(savedInstanceState)
+    }
+
+    private fun initFragNavController(savedInstanceState: Bundle?) {
+        fragNavController.apply {
+            createEager = false
+            fragmentHideStrategy = FragNavController.HIDE
+            rootFragments = listOf(SearchMovieFragment())
+            navigationStrategy = UniqueTabHistoryStrategy(object : FragNavSwitchController {
+                override fun switchTab(index: Int, transactionOptions: FragNavTransactionOptions?) {
+                    selectTab(index)
+                }
+            })
+        }
+        fragNavController.initialize(SEARCH_TAB, savedInstanceState)
+        val initial = savedInstanceState == null
+        if (initial) {
+            binding.mainBottomBar.itemActiveIndex = 0
+        }
+        binding.mainBottomBar.onItemSelected = {
+            when (it) {
+                0 -> fragNavController.switchTab(SEARCH_TAB)
+            }
+        }
+        binding.mainBottomBar.onItemReselectedListener = object : OnItemReselectedListener {
+            override fun onItemReselect(pos: Int) {
+                fragNavController.clearStack()
+            }
+        }
+    }
+
+    private fun selectTab(index: Int) {
+        binding.mainBottomBar.itemActiveIndex = index
+        fragNavController.switchTab(index)
+    }
+
+    override fun onBackPressed() {
+        if (fragNavController.popFragment().not())
+            super.onBackPressed()
+    }
+
+    override fun fragNavController() = fragNavController
+
+    companion object {
+
+        private const val SEARCH_TAB = FragNavController.TAB1
     }
 }
--- app/src/main/java/io/github/masterj3y/mymovie/core/platform/BaseFragment.kt
+++ app/src/main/java/io/github/masterj3y/mymovie/core/platform/BaseFragment.kt
@@ -1,5 +1,6 @@
 package io.github.masterj3y.mymovie.core.platform
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,11 +9,21 @@ import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
+import com.ncapdevi.fragnav.FragNavController
+import com.ncapdevi.fragnav.FragNavTransactionOptions
 
 abstract class BaseFragment<B : ViewDataBinding>(@LayoutRes private val layoutRes: Int) :
     Fragment() {
 
     lateinit var binding: B
+
+    private var fragNavController: FragNavController? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is FragmentNavigationHost)
+            fragNavController = context.fragNavController()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,5 +32,12 @@ abstract class BaseFragment<B : ViewDataBinding>(@LayoutRes private val layoutRe
     ): View? {
         binding = DataBindingUtil.inflate(layoutInflater, layoutRes, container, false)
         return binding.root
+    }
+
+    fun pushFragment(fragment: Fragment, transactionOptions: FragNavTransactionOptions? = null) =
+        fragNavController?.pushFragment(fragment, transactionOptions)
+
+    interface FragmentNavigationHost {
+        fun fragNavController(): FragNavController
     }
 }
