946a842b5e57690e8bd03a03c4329fca153f59c9
Mohammad Jahangiry <mjahangiry75@gmail.com>
2020-08-23 20:32:57 +0430

    Add BaseFragment

---
+++ app/src/main/java/io/github/masterj3y/mymovie/core/platform/BaseFragment.kt
@@ -0,0 +1,25 @@
+package io.github.masterj3y.mymovie.core.platform
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<B : ViewDataBinding>(@LayoutRes private val layoutRes: Int) :
+    Fragment() {
+
+    lateinit var binding: B
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(layoutInflater, layoutRes, container, false)
+        return binding.root
+    }
+}
