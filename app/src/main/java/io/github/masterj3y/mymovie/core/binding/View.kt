1d6337b48a3e8becbe291af00be776f8c87562b3
Mohammad Jahangiry <mjahangiry75@gmail.com>
2020-08-30 11:36:15 +0430

    Revert "Add Glide Palette to Watchlist Item"
    This reverts commit 51dfc458

--- app/src/main/java/io/github/masterj3y/mymovie/core/binding/View.kt
+++ app/src/main/java/io/github/masterj3y/mymovie/core/binding/View.kt
@@ -2,38 +2,14 @@ package io.github.masterj3y.mymovie.core.binding
 
 import android.view.View
 import android.widget.ImageView
-import androidx.cardview.widget.CardView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.github.florent37.glidepalette.BitmapPalette
-import com.github.florent37.glidepalette.GlidePalette
 import io.github.masterj3y.mymovie.core.extension.loadFromUrl
 
 @BindingAdapter("android:loadImageFromUrl")
 fun loadImageFromUrl(view: ImageView, url: String?) {
     url?.let { view.loadFromUrl(it) }
 }
-
-@BindingAdapter("android:loadImageFromUrl", "android:loadBackgroundPalette")
-fun loadImageFromUrl(view: ImageView, url: String?, paletteView: CardView) {
-    url?.let {
-        Glide.with(view.context)
-            .load(url)
-            .listener(
-                GlidePalette.with(url)
-                    .use(BitmapPalette.Profile.VIBRANT_DARK)
-                    .intoCallBack { palette ->
-                        val rgb = palette?.dominantSwatch?.rgb
-                        if (rgb != null) {
-                            paletteView.setCardBackgroundColor(rgb)
-                        }
-                    }
-                    .crossfade(true))
-            .into(view)
-    }
-}
-
 
 @BindingAdapter("android:gone")
 fun gone(view: View, gone: Boolean) {
